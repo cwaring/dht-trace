caa096b5561e20ad1c881be61189989ee11f3c6c
Chris Waring <chris@wwaves.co>
2019-06-28 19:34:53 +0200

    render data

--- src/components/QueryTable/index.jsx
+++ src/components/QueryTable/index.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import styled from "styled-components";
-import { Box } from "../System";
+import { Box, Text } from "../System";
 
-const QuerySection = ({ QueryID }) => <Box>QuerySection: {QueryID}</Box>;
+const QuerySection = ({ QueryID, RunnerState, PeerQueries }) => (
+  <Box>
+    <code>
+      <Text>QuerySection: {QueryID}</Text>
+      <Text>RunnerState: {RunnerState.PeersSeen}</Text>
+    </code>
+    <QueryRow>
+      {PeerQueries.map(peerQuery => (
+        <QueryPeerRow {...peerQuery} />
+      ))}
+    </QueryRow>
+  </Box>
+);
 const QueryRow = ({ children }) => <Box>QueryRow: {children}</Box>;
-const QueryPeerRow = ({ children }) => <Box>QueryPeerRow: {children}</Box>;
+const QueryPeerRow = ({ children, ...rest }) => (
+  <Box>
+    QueryPeerRow: {children}, <code>{JSON.stringify(rest)}</code>
+  </Box>
+);
 
 export default ({ data }) => (
   <Box p={4}>
     {console.log(data)}
     {data && data.map(q => <QuerySection key={q.QueryID} {...q} />)}
-    <QuerySection>
-      <QueryRow>
-        <QueryPeerRow />
-      </QueryRow>
-    </QuerySection>
   </Box>
 );
